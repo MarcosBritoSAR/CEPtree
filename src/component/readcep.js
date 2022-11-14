81dfa4a7b4b9fc65dd463966cd0ad3b3bd33c644
Joao Vinicius <joaofirmino@aluno.uespi.br>
2022-11-14 10:13:09 -0300

    segunda

--- src/component/readcep.js
+++ src/component/readcep.js
@@ -1,5 +1,7 @@
 import fs from 'vite-plugin-fs/browser';
 
+import {Address} from '../core/Address';
+
 export async function getDataObject(size = -1, tree) {
     var csv = await fs.readFile('public/ceps.csv')
     var array = csv.toString().split("\n");
@@ -22,6 +24,16 @@ export async function getDataObject(size = -1, tree) {
             obj[headers[j]] = (str[item] !== undefined) ? str[item].trim() : ""
             item += 1
         }
+
+        var address_example = new Address({
+            city: obj["CIDADE"],
+            street: obj["ENDEREÇO"],
+            neighborhood: obj["BAIRRO"],
+            zipCode: obj["CEP"],
+            complement: obj["COMPLEMENTO"]
+        })
+        tree.insert(address_example)
+
         result.push(obj)
     }
 
